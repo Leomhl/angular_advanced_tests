babc686eaa6b4620275a0db340601b312e6a2657
Leonardo Marinho <leonardo.marinho@everi.com>
2021-11-10 15:49:37 -0300

    Debounce test implementation

--- src/app/shared/components/photo-frame/photo-frame.component.spec.ts
+++ src/app/shared/components/photo-frame/photo-frame.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 
 import { PhotoFrameComponent } from './photo-frame.component';
 
@@ -19,7 +19,25 @@ describe(PhotoFrameComponent.name, () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
   });
+
+  it(`#${PhotoFrameComponent.prototype.like.name} 
+    should trigger (@output liked) once when called multiple 
+    times withing debounce time`, fakeAsync(()=> {
+      fixture.detectChanges();
+      let times = 0;
+
+      component.liked.subscribe(() => times++);
+      component.like();
+      component.like();
+
+      // The clock sounds "tick, tack..." So, this function manipulate the
+      // clock ticks setting a specific time to awai inside a fakeAsync function
+      // Here we need to await 500 milisseconds to consider the debounce time that 
+      // we set inside the observable of photo.frame.component
+      tick(500);
+      expect(times).toBe(1);
+  }));
 });
